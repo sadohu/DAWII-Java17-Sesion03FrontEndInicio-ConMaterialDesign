71d32b6003d065db02cbb15acd9c0faf733e4b7b
sadohu <sadohu26@yahoo.com>
2023-10-30 00:04:48 -0500

    [Add] Save Docente

--- src/app/components/crud-docente-add/crud-docente-add.component.ts
+++ src/app/components/crud-docente-add/crud-docente-add.component.ts
@@ -6,6 +6,7 @@ import { Ubigeo } from 'src/app/models/ubigeo.model';
 import { DocenteService } from 'src/app/services/docente.service';
 import { UbigeoService } from 'src/app/services/ubigeo.service';
 import Swal from 'sweetalert2'
+import { CrudDocenteComponent } from '../crud-docente/crud-docente.component';
 
 @Component({
   selector: 'app-crud-docente-add',
@@ -30,7 +31,11 @@ export class CrudDocenteAddComponent {
     },
   };
 
-  constructor(private ubigeoService: UbigeoService){
+  constructor(
+      private ubigeoService: UbigeoService,
+      private docenteService : DocenteService,
+      private formBuilder: FormBuilder,
+      private dialogRef : MatDialogRef<CrudDocenteComponent>){
     this.ubigeoService.listarDepartamento().subscribe(
       response => this.departamentos = response
     );
@@ -50,6 +55,32 @@ export class CrudDocenteAddComponent {
       response =>  this.distritos = response
     );
     this.docente.ubigeo!.idUbigeo = -1;
+  }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+  registra(){
+    this.docenteService.inserta(this.docente).subscribe(
+      x => {
+        // una vez que inserto, muestro el mensaje
+        Swal.fire('Mensaje', x.mensaje, 'info');
+        // luego borramos todo el  json, es decir, inicializamos las variables
+        this.docente = {
+          idDocente: 0,
+          nombre: "",
+          dni: "",
+          estado: 1,
+          ubigeo: {
+            idUbigeo: -1,
+            departamento: "-1",
+            provincia: "-1",
+            distrito: "-1",
+          }
+        };
+      }
+    );
   }
 
 }
